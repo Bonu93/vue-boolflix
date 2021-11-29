98d2d85a7e67695c81e2b62cd2f16bf4dc3e8e02
Bonu93 <luca.morucci.kb@gmail.com>
2021-11-29 13:40:55 +0100

    complete milestone 1

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,39 +1,23 @@
 <template>
-  <main>
-      <div class="movies">
-          <ul 
-          v-for="(movie, i) in moviesList"
-          :key="`movie-${i}`"
-          >
-
-            <li>
-                Titolo: {{ movie.title }}
-            </li>
-             <li>
-                Titolo originale: {{ movie.original_title }}
-            </li>
-             <li>
-                Lingua: {{ movie.original_language }}
-            </li>
-             <li>
-                Voto: {{ movie.vote_average }}
-            </li>
-
-
-          </ul>
-      </div>
-  </main>
+    <main>
+        <div class="movies">
+            <ul v-for="(movie, i) in moviesList" :key="`movie-${i}`">
+                <li>Titolo: {{ movie.title }}</li>
+                <li>Titolo originale: {{ movie.original_title }}</li>
+                <li>Lingua: {{ movie.original_language }}</li>
+                <li>Voto: {{ movie.vote_average }}</li>
+            </ul>
+        </div>
+    </main>
 </template>
 
 <script>
 export default {
-    name: 'Main',
+    name: "Main",
     props: {
         moviesList: Array,
-    }
-}
+    },
+};
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
