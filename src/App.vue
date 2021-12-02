0c6edf2c2bc43a1a8933fb57b6ba6d78b07eb237
Bonu93 <luca.morucci.kb@gmail.com>
2021-12-02 15:09:43 +0100

    add popular items to homepage

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,9 @@
   <div id="app" class="h-100 bg-prim d-flex flex-column">
     <Header @search="getResearch" />
     <main class="overflow-auto flex-grow-1 text-light p-4">
+      <h2>FILM</h2>
       <Gallery :list="movies"/>
-
+      <h2>SERIE TV</h2>
       <Gallery :list="series"/>
 
     </main>
@@ -31,10 +32,40 @@ export default {
   },
 
   created() {
-
+    this.getPopMovies();
+    this.getPopSeries();
   },
 
   methods: {
+    
+    getPopSeries ()  {
+      axios.get('https://api.themoviedb.org/3/tv/popular', {
+        params: {
+          api_key: '97334a30f0eef5e4d8fc3009e75f736b',
+                language: 'it',
+                page: 1,
+                
+        }
+      })
+      .then((result) => {
+        this.series = result.data.results;
+      })
+    },
+
+    getPopMovies() {
+      axios.get('https://api.themoviedb.org/3/movie/popular', {
+        params: {
+          api_key: '97334a30f0eef5e4d8fc3009e75f736b',
+                language: 'it',
+                page: 1,
+                
+        }
+      })
+      .then((result) => {
+        this.movies = result.data.results;
+      })
+        
+    },
     getMovies() {
       axios
         .get('https://api.themoviedb.org/3/search/movie' , {
@@ -78,6 +109,10 @@ export default {
 </script>
 
 <style lang="scss">
-  @import '@/styles/globals.scss'
+  @import '@/styles/globals.scss';
+
+  main {
+    text-align: center;
+  }
 
 </style>
